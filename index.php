b318a6c7d19570748214c5bc0580f9f401610123
elribonazo <elribonazo@gmail.com>
2015-07-05 03:31:55 +0200

    Enabling errors

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-	error_reporting(0);
+	error_reporting(E_ALL);
 	require_once(dirname(__FILE__) . "/rif/loader/autoloader.php");
 	autoloader::autoload();
 	$riframework = new riframework();
